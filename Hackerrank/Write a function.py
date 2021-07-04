befec2c638272ab9358e33ea7149087563e8fe7d
siddhibhor-56 <65079414+siddhibhor-56@users.noreply.github.com>
2021-07-04 21:02:34 +0530

    Add files via upload

---
+++ Hackerrank/Write a function.py
@@ -0,0 +1,17 @@
+Q=https://www.hackerrank.com/challenges/write-a-function/problem
+
+def is_leap(year):
+    leap = False
+    if (year%4)==0:
+        if year%400==0 and year%100!=0:
+            leap = True
+        else:
+            leap = False
+    else:
+        leap = False
+    return leap
+            
+    
+
+year = int(input())
+print(is_leap(year))
